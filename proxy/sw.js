2c3c2b0e193b264d04c850b7974cf0d8274e6467
obsidiangame <fuckkiroai2@gmail.com>
2026-08-09 19:45:39 -0500

    Fix proxy first-load: don't wait for controllerchange (page is out of SW scope); SW claims + skipWaits so frames get proxied immediately

--- proxy/sw.js
+++ proxy/sw.js
@@ -1,5 +1,13 @@
 importScripts("./controller/controller.sw.js");
 
+addEventListener("install", (e) => {
+	e.waitUntil(skipWaiting());
+});
+
+addEventListener("activate", (e) => {
+	e.waitUntil(clients.claim());
+});
+
 addEventListener("fetch", (e) => {
 	if ($scramjetController.shouldRoute(e)) {
 		e.respondWith($scramjetController.route(e));
